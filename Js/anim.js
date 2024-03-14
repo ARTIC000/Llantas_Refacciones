4e45bbfa5d442d8e002d3079c775297858dbccbb
ARTIC000 <faridy.rr.26@gmail.com>
2024-03-13 22:41:03 -0600

    Landing Update

--- Js/anim.js
+++ Js/anim.js
@@ -27,3 +27,51 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('scroll', revealOnScroll);
+  
+    function revealOnScroll() {
+      var workingspace = document.querySelector('.table-content');
+  
+      var bounding = workingspace.getBoundingClientRect();
+      
+      if (bounding.top <= window.innerHeight - 100) {
+        workingspace.classList.add('show');
+      }
+    }
+  });
+
+
+
+//   Circle
+const circles = document.querySelectorAll('.circle');
+const infoContainer = document.querySelector('.info-container');
+const infoTexts = document.querySelectorAll('.info-text');
+
+circles.forEach((circle, index) => {
+  circle.addEventListener('click', () => {
+    infoContainer.style.display = 'block';
+    infoTexts.forEach((text, i) => {
+      if (i === index) {
+        text.style.display = 'block';
+      } else {
+        text.style.display = 'none';
+      }
+    });
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('scroll', revealOnScroll);
+  
+    function revealOnScroll() {
+      var workingspace = document.querySelector('.circle-container');
+  
+      var bounding = workingspace.getBoundingClientRect();
+      
+      if (bounding.top <= window.innerHeight - 100) {
+        workingspace.classList.add('show');
+      }
+    }
+  });
